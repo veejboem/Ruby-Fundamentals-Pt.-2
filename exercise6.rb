d647cd58d6e6db65caca600e746e9fb6c667f087
Valerie Boem <vboem@gmail.com>
2017-01-17 16:21:26 -0500

    Created method.

--- exercise6.rb
+++ exercise6.rb
@@ -1,9 +1,17 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-  grocery_list. each do |x|
-  puts "* " + x
+  grocery_list.each do |x|
+  puts "* #{x}"
 end
 
 grocery_list << "rice"
-  grocery_list. each do |x|
+  grocery_list.each do |x|
   puts "* " + x
 end
+
+def food_items(y)
+  y.each do |a|
+  puts "* #{a}"
+end
+end 
+
+food_items(grocery_list)
