ac0b0c945818a8d12a1f9c3c225bad4819151d3e
Valerie Boem <vboem@gmail.com>
2017-01-17 15:44:01 -0500

    Added rice to list.

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,9 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
   grocery_list. each do |x|
-    puts "* " + x
+  puts "* " + x
+end
+
+grocery_list << "rice"
+  grocery_list. each do |x|
+  puts "* " + x
 end
