91485e2f2fba65260d5d62487147a6f3825e3b02
Valerie Boem <vboem@gmail.com>
2017-01-17 17:13:49 -0500

    Deleted salmon from the list.

--- exercise5.rb
+++ exercise5.rb
@@ -6,3 +6,9 @@ end
 puts "What is the temperature in fahrenheit?"
 f = gets.chomp.to_i
 f_to_c(f)
+
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+  grocery_list << "rice"
+  grocery_list. each do |x|
+  puts "* " + x
+end
--- exercise6.rb
+++ exercise6.rb
@@ -1,29 +1,40 @@
+#Part 1 (create list with "*")
+
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
   grocery_list.each do |x|
   puts "* #{x}"
 end
 
+#Part 2 (add rice and reprint list)
 grocery_list << "rice"
   grocery_list.each do |x|
   puts "* " + x
 end
 
+#create method
 def food_items(y)
   y.each do |a|
   puts "* #{a}"
   end
 end
 
+#Part 4 (count items in list)
 food_items(grocery_list)
-
 puts "There are #{grocery_list.length} items on your list."
 
+#Part 5 (Pick up banana reminder)
 if grocery_list.include?("bananas")
   puts "You don't need to pick up bananas today."
 elsif puts "You need to pick up bananas!"
 end
 
+#Part 6 (display second item in list)
 p grocery_list[1]
 
+#Part 7 (Sort list alphabetically)
 grocery_list = grocery_list.sort
+food_items(grocery_list)
+
+#Part 8 (Remove salmon from the list)
+grocery_list.delete("salmon")
 food_items(grocery_list)
